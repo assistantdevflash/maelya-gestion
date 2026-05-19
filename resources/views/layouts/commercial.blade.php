9af90fa742831e9e157747fd66435fc46c489ede
assistantdevflash <assistant_dev@flashintervention.com>
2026-05-19 09:52:57 +0000

    fix: retirer onglet Profil de la bottom nav (accessible via dropdown)

--- resources/views/layouts/commercial.blade.php
+++ resources/views/layouts/commercial.blade.php
@@ -42,7 +42,6 @@
         ['route' => 'commercial.parrainages', 'icon' => 'M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0zm6 3a2 2 0 11-4 0 2 2 0 014 0zM7 10a2 2 0 11-4 0 2 2 0 014 0z', 'label' => 'Mes parrainages',   'short' => 'Parrainages'],
         ['route' => 'commercial.commissions', 'icon' => 'M9 14l6-6m-5.5.5h.01m4.99 5h.01M19 21V5a2 2 0 00-2-2H7a2 2 0 00-2 2v16l3.5-2 3.5 2 3.5-2 3.5 2z',                                       'label' => 'Mes commissions', 'short' => 'Commissions'],
         ['route' => 'commercial.guide',       'icon' => 'M9 12h6m-6 4h6m2 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z',               'label' => 'Guide Porte-à-Porte', 'short' => 'Guide'],
-        ['route' => 'commercial.profil',      'icon' => 'M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z',                                                                   'label' => 'Mon profil',          'short' => 'Profil'],
     ];
     @endphp
 
