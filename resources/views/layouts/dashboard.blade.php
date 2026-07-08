0ef13302940699ff0071492d1a1e35d3f9de1866
assistantdevflash <assistant_dev@flashintervention.com>
2026-07-08 14:59:44 +0000

    🎨 Update: rename "Boutique" to "Boutique en ligne" in dashboard layout

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -391,7 +391,7 @@
                         <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.8">
                             <path d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"/>
                         </svg>
-                        <span class="flex-1 text-left">Boutique</span>
+                        <span class="flex-1 text-left">Boutique en ligne</span>
                         @if($nouvellesCommandes > 0)
                         <span class="inline-flex items-center justify-center w-5 h-5 text-[10px] font-bold text-white bg-pink-500 rounded-full">{{ $nouvellesCommandes }}</span>
                         @endif
