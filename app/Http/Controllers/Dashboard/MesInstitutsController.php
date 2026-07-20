8e5599fba741110b1f99cb0227839028a835d4d2
LeProlifique <mac@MacBook-Pro-de-Mac.local>
2026-07-20 13:34:22 +0000

    🐛 Fix: validation update() MesInstitutsController manquait informatique_telephonie

--- app/Http/Controllers/Dashboard/MesInstitutsController.php
+++ app/Http/Controllers/Dashboard/MesInstitutsController.php
@@ -95,7 +95,7 @@ class MesInstitutsController extends Controller
             'ville'     => ['nullable', 'string', 'max:100'],
             'telephone' => ['nullable', 'string', 'max:20'],
             'email'     => ['nullable', 'email', 'max:150'],
-            'type'      => ['required', 'string', 'in:salon_coiffure,institut_beaute,barbier,centre_esthetique,boutique_mode,imprimerie,lavage_auto,pressing,business_center,depot_gaz,commerce,autre'],
+            'type'      => ['required', 'string', 'in:salon_coiffure,institut_beaute,barbier,centre_esthetique,boutique_mode,imprimerie,lavage_auto,pressing,business_center,depot_gaz,commerce,informatique_telephonie,autre'],
         ]);
 
         $institut->update($data);
